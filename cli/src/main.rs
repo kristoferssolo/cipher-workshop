46a5b90d344912c428ab69907f254bfcf3c84384
Kristofers Solo <dev@kristofers.xyz>
2025-10-17 21:45:17 +0300

    feat(cli): add basic cli feature

--- cli/src/main.rs
+++ cli/src/main.rs
@@ -1,3 +1,29 @@
-fn main() {
-    println!("Hello, world!");
+mod args;
+
+use crate::args::{Args, Operation, OutputFormat};
+use cipher_core::BlockCipher;
+use clap::Parser;
+use des::Des;
+
+fn main() -> anyhow::Result<()> {
+    let args = Args::parse();
+    let des = Des::new(args.key.as_64());
+
+    match args.operation {
+        Operation::Encrypt => {
+            let ciphertext = des.encrypt(&args.text.to_be_bytes())?;
+            println!("{ciphertext:016X}");
+        }
+        Operation::Decrypt { output_format } => {
+            let plaintext = des.decrypt(&args.text.to_be_bytes())?;
+            match output_format.unwrap_or_default() {
+                OutputFormat::Binary => println!("{plaintext:064b}"),
+                OutputFormat::Octal => println!("{plaintext:022o}"),
+                OutputFormat::Decimal => println!("{plaintext}"),
+                OutputFormat::Hex => println!("{plaintext:016X}"),
+                OutputFormat::Text => println!("{plaintext}"),
+            }
+        }
+    }
+    Ok(())
 }
