3231cea0f269e26101fa39a88a2f667caa405b42
Kristofers Solo <dev@kristofers.xyz>
2025-10-18 14:36:27 +0300

    feat(tui): copy add basic event driven template

--- tui/src/main.rs
+++ tui/src/main.rs
@@ -1,3 +1,13 @@
-fn main() {
-    println!("Hello, world!");
+use crate::app::App;
+
+mod app;
+mod event;
+mod ui;
+
+fn main() -> color_eyre::Result<()> {
+    color_eyre::install()?;
+    let terminal = ratatui::init();
+    let result = App::new().run(terminal);
+    ratatui::restore();
+    result
 }
